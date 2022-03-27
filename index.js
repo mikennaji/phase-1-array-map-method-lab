b87cf70073504f848a510b4bc3f59c1b0ad1a20f
mikennaji <mnnaji3@uwo.ca>
2022-03-27 10:31:01 -0400

    commit tree b642dbad1b8d8f0bf00c5487acd41dd4f45ac374

--- index.js
+++ index.js
@@ -11,6 +11,65 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+/**const titleCased = () => {
+  let newtutorials= [...tutorials];
+  let count = 0;
+  newtutorials.forEach(sentence => {
+    let space = [];
+      for(let letters in sentence){
+        
+        if(sentence[letters]===" "){
+          const position = parseInt(letters) + 1;
+          //const stringposition = String(position);
+          space.push(position)
+
+      };
+ 
+  }
+ console.log(space);
+
+  let copy = [...space]
+  copy: {
+    writable: true
+  }
+  //consolesentence[space[0]]
+  
+  for(let counter in copy){
+    console.log(newtutorials[count],copy[counter],sentence[copy[counter]])
+  
+    setElementDestructively(newtutorials[count],copy[counter],sentence[copy[counter]])
+    console.log(newtutorials[count])
+  }
+  count++;
+  
+  //for (let count in space){
+    //sentence[count] = sentence[count].toUpperCase();
+  //}
+
+
+
+
+});
+  return newtutorials
+}
+ 
+function setElementDestructively(arr, index, value) {
+  let replacement = value.toUpperCase();
+  arr[index] = replacement;
+  console.log(replacement)
+  return arr
+}
+**/
+
+const titleCased = (input) => {
+  return tutorials.map( line => {
+    const tokens = line.split(' ');
+    console.log(tokens);
+    const capitalizedTokens =
+      tokens.map( token => token.charAt(0).toUpperCase() + token.slice(1) );
+      console.log(capitalizedTokens);
+    const response = capitalizedTokens.join(' ');
+    return response;
+  });
 }
